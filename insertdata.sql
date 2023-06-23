c471eaa720d1bf7bb8528fe941ad87858332440d
phil <phil@email.com>
2023-06-23 15:29:29 +0100

    added data to db

--- insertdata.sql
+++ insertdata.sql
@@ -1,6 +1,23 @@
 USE gamesdb;
 
 INSERT INTO customers (fname, lname, age, contact)
-VALUES ('john', 'smith', 22, 07710123123);
+VALUES ('steve', 'rogers', 29, 07710123123),
+('tony', 'stark', 31, 07710246810),
+('pter', 'parker', 22, 077172225610),
+('bruce', 'wayne', 35, 07719567123),
+('clark', 'kent', 27, 07712336699);
+
+INSERT INTO products (title, genre, price)
+VALUES ('call of duty', 'FPS', 45.00),
+('assassins creed', 'RPG', 35.00),
+('FIFA 2023', 'Sports', 40.00),
+('GTA 5', 'FPS', 30.00);
+
+INSERT INTO orders (dateofpurchase, custid, prodid)
+VALUES (20230601, 1, 2),
+(20230612, 2, 4),
+(20230610, 3, 1);
 
 SELECT * FROM customers;
+SELECT * FROM products;
+SELECT * FROM orders;
