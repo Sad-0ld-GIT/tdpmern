f7965a1dd80ef1b7d5caf75871dd1da4b12ee80b
user <user@email.com>
2023-07-21 17:54:57 +0100

    exp sql ex

---
+++ backend/express/mySQL/server.js
@@ -0,0 +1,60 @@
+let express = require("express");
+let server = express();
+let mysql = require("mysql");
+
+let db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"katnbs"
+});
+
+db.connect();
+
+server.get("/", function(req,res){
+    res.render("homepage.ejs");
+    res.end()
+})
+
+server.get("/allemployees", function(req,res){
+    db.query("select * from employees", function(err, data){
+    res.render("employeelist.ejs", {employees:data});
+    res.end()
+   })
+})
+
+server.get("/departmentEmployees/:dept", function(req,res){
+    let sql = "select * from employees where department='"+req.params.dept+"'"
+    db.query(sql, function(err, data){
+    res.render("employeelist.ejs", {employees:data});
+    res.end()
+   })
+})
+
+server.get("/departments", function(req,res){
+    db.query("select distinct department from employees", function(err, data){
+    res.render("departments.ejs", {deptName:data});
+    res.end()
+    })
+})
+
+server.get("/deleteEmployee/:empno", function(req,res){
+    let deleteSQL="delete from employees where empno="+req.params.empno
+    db.query(deleteSQL,function(err, data){
+
+        db.query("select * from employees",function(err, data){
+            res.render("employeelist.ejs",{employees:data})
+            res.end()
+        })
+    })
+})
+
+server.get("/show", function(req,res){
+    db.query("select * from employees", function(err,data){
+        console.log(data)
+    })
+    res.write("Hello")
+    res.end()
+});
+
+server.listen(3000)
