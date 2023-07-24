a7c47abe8ce6d1af0c4a86e2cbd7b395d5a67f8c
Sad-0ld-GIT <136621076+Sad-0ld-GIT@users.noreply.github.com>
2023-07-24 11:06:53 +0100

    Update server.js

--- backend/express/mySQL/server.js
+++ backend/express/mySQL/server.js
@@ -1,6 +1,9 @@
 let express = require("express");
 let server = express();
 let mysql = require("mysql");
+let bodyparser=require("body-parser")
+server.use(bodyparser.json())
+server.use(bodyparser.urlencoded({ extended: true }));
 
 let db = mysql.createConnection({
     host:"localhost",
@@ -10,6 +13,25 @@ let db = mysql.createConnection({
 });
 
 db.connect();
+
+server.get("/addEmployee",function(re,res){
+    res.render("Form.ejs")
+})
+
+server.post("/saveEmployee",function(req,res){
+    let empno=req.body.empno
+    let name=req.body.ename
+    let salary=req.body.esal
+    let dept=req.body.edept
+    let insertquery=`insert into employees values(${empno},'${name}',${salary},'${dept}')`
+    db.query(insertquery,function(err,data){
+        console.log(err)
+        db.query("select * from employees",function(err,records){
+            res.render("employeeslist.ejs",{employees:records})
+            res.end()
+        })
+    })
+})
 
 server.get("/", function(req,res){
     res.render("homepage.ejs");
